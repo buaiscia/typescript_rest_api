c1ab88cd8b0abc87f035455883c2ade489d5afb3
Alex Buaiscia (HCI) <alex.buaiscia@gmail.com>
2020-05-10 10:45:53 +0200

    converted server file to ts

---
+++ dist/api/server.js
@@ -0,0 +1,95 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const mongoose_1 = __importDefault(require("mongoose"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const morgan_1 = __importDefault(require("morgan"));
+// const express = require("express");
+// const mongoose = require("mongoose");
+// const bodyParser = require("body-parser");
+// const logger = require("morgan");
+const app = express_1.default();
+const swagger_ui_express_1 = __importDefault(require("swagger-ui-express"));
+const openapi_json_1 = __importDefault(require("./openapi.json"));
+// const swaggerUi = require('swagger-ui-express');
+// const openApiDocumentation = require('./openapi.json');
+app.use('/api-docs', swagger_ui_express_1.default.serve, swagger_ui_express_1.default.setup(openapi_json_1.default));
+// CONFIGURATION
+app.use(express_1.default.static(__dirname + "/view"));
+app.use(express_1.default.static(__dirname + "/api/public"));
+app.use(express_1.default.json());
+app.use(body_parser_1.default.json());
+app.use(body_parser_1.default.urlencoded({ extended: true }));
+app.use(morgan_1.default("dev"));
+//  MONGODB SETUP
+let url = '';
+if (process.env.DATABASEURL) {
+    url = process.env.DATABASEURL;
+    mongoose_1.default.connect(url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+        .then(() => {
+        console.log("Database connection successful");
+    })
+        .catch(err => {
+        console.error("Database connection error: " + err);
+    });
+}
+else {
+    url = "mongodb://mongo:27017"; // TO USE WITH DOCKER
+    // url = "mongodb://localhost:27017"; // TO USE LOCALLY
+    const dbName = "moviesDB";
+    mongoose_1.default
+        .connect(`${url}/${dbName}`, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+        .then(() => {
+        console.log("Database connection successful");
+    })
+        .catch(err => {
+        console.error("Database connection error: " + err);
+    });
+}
+//SETUP HEADERS
+app.use((req, res, next) => {
+    const allowed = "*";
+    res.header("Access-Control-Allow-Origin", allowed);
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Authorization");
+    if (req.method === "OPTIONS") {
+        res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE");
+        return res.status(200).json({});
+    }
+    next();
+});
+// var http = require("http");
+// setInterval(function () {
+//     http.get("https://restful-db-api.herokuapp.com/");
+// }, 300000); // every 5 minutes (300000)
+// SET ROUTES
+const index_1 = __importDefault(require("./api/routes/index"));
+const upload_1 = __importDefault(require("./api/routes/upload"));
+// const indexRoute = require("./api/routes/index");
+// const uploadRoute = require("./api/routes/upload");
+// USE ROUTES
+app.use("/upload", upload_1.default);
+app.use("/", index_1.default);
+//ERROR/404 ROUTES
+app.use((err, req, res, next) => {
+    err = new Error("Page not found");
+    err.status = 404;
+    next(err);
+});
+app.use((err, req, res) => {
+    res.status(err.status || 500);
+    res.json({
+        error: { message: err.message }
+    });
+});
+// SERVER
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port} ...`));
---
+++ src/server.ts
@@ -0,0 +1,125 @@
+import express, { Request, Response, NextFunction } from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import logger from "morgan";
+
+// const express = require("express");
+// const mongoose = require("mongoose");
+// const bodyParser = require("body-parser");
+// const logger = require("morgan");
+
+const app = express();
+
+
+import swaggerUi from "swagger-ui-express";
+import openApiDocumentation from "./openapi.json";
+
+
+// const swaggerUi = require('swagger-ui-express');
+// const openApiDocumentation = require('./openapi.json');
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openApiDocumentation));
+
+// INTERFACES
+
+interface Error {
+    status?: number;
+    message?: string;
+}
+
+// CONFIGURATION
+
+app.use(express.static(__dirname + "/view"));
+app.use(express.static(__dirname + "/api/public"));
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(logger("dev"));
+
+//  MONGODB SETUP
+
+let url: string = '';
+
+if(process.env.DATABASEURL) {
+    url = process.env.DATABASEURL;
+    mongoose.connect(url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log("Database connection successful");
+    })
+    .catch(err => {
+        console.error("Database connection error: " + err);
+    });
+}
+else {
+    url = "mongodb://mongo:27017";  // TO USE WITH DOCKER
+    // url = "mongodb://localhost:27017"; // TO USE LOCALLY
+    const dbName: string = "moviesDB";
+    mongoose
+    .connect(`${url}/${dbName}`, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log("Database connection successful");
+    })
+    .catch(err => {
+        console.error("Database connection error: " + err);
+    });
+}
+
+//SETUP HEADERS
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+    const allowed: string = "*";
+    res.header("Access-Control-Allow-Origin", allowed);
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Authorization"
+    );
+    if (req.method === "OPTIONS") {
+        res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE");
+        return res.status(200).json({});
+    }
+    next();
+});
+
+// var http = require("http");
+// setInterval(function () {
+//     http.get("https://restful-db-api.herokuapp.com/");
+// }, 300000); // every 5 minutes (300000)
+
+// SET ROUTES
+
+import indexRoute from "./api/routes/index";
+import uploadRoute from "./api/routes/upload";
+
+// const indexRoute = require("./api/routes/index");
+// const uploadRoute = require("./api/routes/upload");
+
+// USE ROUTES
+
+app.use("/upload", uploadRoute);
+app.use("/", indexRoute);
+
+//ERROR/404 ROUTES
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    err = new Error("Page not found");
+    err.status = 404;
+    next(err);
+});
+
+app.use((err: Error, req: Request, res: Response) => {
+    res.status(err.status || 500);
+    res.json({
+        error: { message: err.message }
+    });
+});
+
+// SERVER
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port} ...`));
